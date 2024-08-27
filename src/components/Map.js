72158be7683212b9c13380506456aba954ecb1ed
MuhammadFikri <udinsaprudin1456@gmail.com>
2024-08-27 16:01:34 +0700

    changing maps to satellite view

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,3 @@
-// src/components/Map.js
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -6,13 +5,13 @@ import 'leaflet/dist/leaflet.css';
 const Map = ({ vessels }) => {
   return (
     <MapContainer 
-      center={[-2.5, 118]} // Center the map on Indonesia
-      zoom={5}             // Adjust the zoom level to cover Indonesia
+      center={[-2.5, 118]} 
+      zoom={5} 
       style={{ height: '100vh' }}
     >
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmlrei0xODQ1IiwiYSI6ImNtMGM2eXdoaTAwMncyaXM3cWNqd2NqM20ifQ.qqDdZe5sneVax6hozsqymQ"
+        attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> contributors'
       />
       {vessels.map(vessel => (
         <Marker key={vessel.mmsi} position={[vessel.lat, vessel.lon]}>
